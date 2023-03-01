e5f1d22ef71f3d2247f27c4f3d122e5812e24d23
Danilo Alfa <dalfahenrique@gmail.com>
2023-03-01 20:37:33 -0300

    MUI

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import {Button, Container, Typography, Autocomplete, TextField} from '@mui/material'
+
+function App() {
+// const opcoes = [
+//   {label: 'opcao 2', number: 2},
+//   {label: 'opcao 1', number: 5},
+//   {label: 'opcao 3', number: 3}
+// ]
+
+  
+
+  return (
+    <Container sx={{bgcolor: 'red' , height: "100vh"}}>
+
+    {/* <Button variant="contained" sx={{bgcolor:"purple", color: "black"}}>Hello World</Button>
+    <Typography variant="h1">Sem ideias do que colocar</Typography>
+    <Autocomplete disablePortal options={opcoes} sx={{width: 300}} renderInput={(params) => <TextField {...params} label = "Movie"> </TextField>} />   */}
+
+    
+    </Container>
+  );
+}
+
+export default App;
